360c394581b2a1fc9a203250a76db5071089998d
TheZeki96 <eric.jara.p@uni.pe>
2025-08-09 14:13:19 -0500

    Update app.py
    Se agregó los super heroes de Marvel

--- API/app.py
+++ API/app.py
@@ -8,16 +8,26 @@ app = FastAPI()
 @app.get("/familia")
 def get_familia():
     rows = ["Amin", "Marce", "Miranda"]
+    
     return rows
 
 
 @app.get("/superheroesDC")
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
+    
     return rows
 
 
 @app.get("/cursosPlatzi")
-def get_superheroes():
+def get_cursoplatzi():
     rows = ["Azure", "Bash", "Python", "GitHub", "Power BI", "Cloud", "AWS", "Terraform"]
+
+    return rows
+
+
+@app.get ("/superheroesMarvel")
+def get_superheroesmarvel():
+    rows = ["Hulk", "Iroman", "Capitan America", "Spiderman"]
+    
     return rows
